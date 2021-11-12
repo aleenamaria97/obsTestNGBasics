ec85256bfc22b003ea3a021b10bf2b4d0aecd861
aleena <aleenamariya97@gmail.com>
2021-11-12 19:30:26 +0530

    xmlFileCreation

--- src/test/java/com/obsqura/testngcommands/HomeTest.java
+++ src/test/java/com/obsqura/testngcommands/HomeTest.java
@@ -3,11 +3,11 @@ package com.obsqura.testngcommands;
 import org.testng.annotations.Test;
 
 public class HomeTest {
-    @Test(priority = 1,enabled = true)
+    @Test(priority = 1,enabled = false)
     public void verifyHomePageTitle(){
         System.out.println("this a home page title");
     }
-    @Test(priority = 2,enabled = true)
+    @Test(priority = 2,enabled = false)
     public void verifyHomePageUserMenu(){
         System.out.println("homepage user menu");
     }
--- src/test/java/com/obsqura/testngcommands/LoginTest.java
+++ src/test/java/com/obsqura/testngcommands/LoginTest.java
@@ -3,11 +3,11 @@ package com.obsqura.testngcommands;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-    @Test(priority = 3,enabled = true)
+    @Test(priority = 3,enabled = false)
     public void verifyLoginPageTitle(){
         System.out.println("this a login page title");
     }
-    @Test(priority = 4,enabled = true)
+    @Test(priority = 4,enabled = false)
     public void verifyValidLogin(){
         System.out.println(" user login");
     }
